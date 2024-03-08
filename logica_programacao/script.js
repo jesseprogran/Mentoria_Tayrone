3c8a97e62a9c88451f7193e911eefe8593703ad2
Jesse Silva <jessepescador22@gmail.com>
2024-03-08 20:03:06 -0300

    realizando exercicio de lógica

---
+++ logica_programacao/script.js
@@ -0,0 +1,67 @@
+// function cadastro() {
+//   let g = 300;
+//   if (g >= 120) {
+//     alert("caminho direto");
+//   } else if (g >= 100 && g < 120) {
+//     alert("abastecer ligeiro");
+//   } else if (g == 30 || g == 40) {
+//     alert("reserva");
+//   } else {
+//     alert("acabou tudo");
+//   }
+// }
+
+// cadastro();
+
+// function cadastro() {
+//   let g = 119;
+//   switch (true) {
+//     case g >= 120:
+//       alert("caminho direto");
+//       break;
+//     case g >= 100 && g < 120:
+//       alert("abastecer ligeiro");
+//       break;
+//     case g == 30 || g == 40:
+//       alert("reserva");
+//       break;
+//     default:
+//       alert("acabou tudo");
+//   }
+// }
+
+// cadastro();
+
+// function cadastro() {
+//   let g = 119;
+//   switch (true) {
+//     case g >= 120:
+//       alert("caminho direto");
+//     case g >= 100 && g < 120:
+//       alert("abastecer ligeiro");
+
+//     case g == 30 || g == 40:
+//       alert("reserva");
+//       break;
+//     default:
+//       alert("acabou tudo");
+//   }
+// }
+
+// cadastro();
+
+// a loja abre as 7 hs da manha porem a fila comeca a ser criada as 6hs,
+//a cada 1 minuto chegade  5 a 10 pessoas
+//crie um algoritmo que calcule o numero de pessoas q terão na fila apos até 7hs .
+// todos produos tem a faixa de preço de 2reais
+
+//o mercado tem a capacidade diaria de armazenar 20mil produtos o mercado
+// ele fecha as 20hs
+// normalmente os cliente gastam de 100 a 500 reais de compra por individuo
+// o mercado tem uma promoção para motivar as pessoas a comprarem mais mercadoria
+// se o cliente compra a cima de 200 reais ele tem 10% de desconto
+// se o cliente compra a cima de 400 reias ele tem 50% de desconto
+// crie um algoritimo que faça a seguinte analise
+
+// quantas venda eu fiz até 20hs, preciso saber tambem se meu estoque zerou antes do fechamento da loja
+// preciso saber tbm quantos cliente tiveream 10% desconto e 50%  de desconto e quanto nao tiveram desconto
